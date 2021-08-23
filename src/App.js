c2242b0642e02af1d7778f93688c4c850f0e5890
Davron Abdukhakimov <dawroun@Davrons-MacBook-Pro.local>
2021-08-23 23:33:46 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { Container, Grid } from '@material-ui/core';
+import Profile from './components/profile/Profile';
+import Header from './components/header/Header';
+import Portfolio from './pages/portfolio/Portfolio';
+import Resume from './pages/resume/Resume';
+import Footer from './components/footer/Footer';
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Container>
+          <Grid container>
+            <Grid item xs={12} sm={12} md={4} lg={3} >
+              <Profile />
+            </Grid>
+            <Grid item xs>
+              <Header />
+              <Router>
+                <Portfolio />
+                <Resume />
+              </Router>
+              <Footer />
+            </Grid>
+          </Grid>
+        </Container>
     </div>
   );
 }
